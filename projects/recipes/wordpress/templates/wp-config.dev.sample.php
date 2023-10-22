157ba80255b0e68a1d77ccb511f6a6ed6626a623
pkosciak <pkosciak@webpacket.io>
2023-10-22 11:35:15 +0200

    Add WordPress configs templates, remove mysql automatic container creation on docker startup

---
+++ projects/recipes/wordpress/templates/wp-config.dev.sample.php
@@ -0,0 +1,13 @@
+<?php
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
+define('WP_DEBUG_DISPLAY', false);
+define('SCRIPT_DEBUG', true);
+
+@ini_set('display_errors', 0);
+
+/** Docker redirection loop fix */
+define('FORCE_SSL_ADMIN', true);
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false){
+    $_SERVER['HTTPS']='on';
+}
