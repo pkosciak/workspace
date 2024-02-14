8a957fcd97753378b48e8e80d11f7be36e0d13b0
pkosciak <pkosciak@webpacket.io>
2024-02-14 08:13:12 +0100

    Update wp-config.dev.sample.php

--- projects/recipes/wordpress/templates/wp-config.dev.sample.php
+++ projects/recipes/wordpress/templates/wp-config.dev.sample.php
@@ -3,12 +3,15 @@ define('WP_DEBUG', true);
 define('WP_DEBUG_LOG', true);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', true);
+define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
 
 @ini_set('display_errors', 0);
 //@ini_set( 'memory_limit', -1 );
 
 /** Docker redirection loop fix */
 define('FORCE_SSL_ADMIN', true);
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false){
-    $_SERVER['HTTPS']='on';
+if(!defined('WP_CLI')){
+    if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false){
+        $_SERVER['HTTPS']='on';
+    }
 }
